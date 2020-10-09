b40ba85a2260e857f2fa4e587e77e48eb7ed0841
M Albert <2231puppy@gmail.com>
2020-10-09 16:21:51 -0400

    Changes to be committed: 	new file:   .github/workflows/build.yml 	new file:   .github/workflows/release.yml 	new file:   .gitignore 	new file:   .pre-commit-config.yaml 	new file:   .pylintrc 	new file:   .readthedocs.yml 	new file:   CODE_OF_CONDUCT.md 	new file:   LICENSE 	new file:   LICENSES/CC-BY-4.0.txt 	new file:   LICENSES/MIT.txt 	new file:   LICENSES/Unlicense.txt 	new file:   README.rst 	new file:   README.rst.license 	new file:   adafruit_discordbot.py 	new file:   docs/_static/favicon.ico 	new file:   docs/_static/favicon.ico.license 	new file:   docs/api.rst 	new file:   docs/api.rst.license 	new file:   docs/conf.py 	new file:   docs/examples.rst 	new file:   docs/examples.rst.license 	new file:   docs/index.rst 	new file:   docs/index.rst.license 	new file:   examples/discordbot_simpletest.py 	new file:   pyproject.toml 	new file:   requirements.txt 	new file:   setup.py

---
+++ adafruit_discordbot.py
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2020 2231puppy for Project Stida
+#
+# SPDX-License-Identifier: MIT
+"""
+`adafruit_discordbot`
+================================================================================
+
+A very simple Discord API for CircuitPython
+
+
+* Author(s): 2231puppy
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s). Use unordered list & hyperlink rST
+   inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://github.com/adafruit/circuitpython/releases
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
+# imports
+import adafruit_requests as requests
+import json
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/2231puppy/Adafruit_CircuitPython_DiscordBot.git"
+
+class DiscordBot:
+    def __init__(self, key):
+        self.key = key
+    def get_msg(self, channel):
+        requests.get("https://discord.com/api/v8/channels/"+channel+"/messages")
