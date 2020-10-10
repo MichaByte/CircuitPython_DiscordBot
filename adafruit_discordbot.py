cf68ea551991cb44f28c393b07a8ca53a17a146e
M Albert <2231puppy@gmail.com>
2020-10-10 12:50:42 -0400

    Changes to be committed: 	modified:   adafruit_discordbot.py

--- adafruit_discordbot.py
+++ adafruit_discordbot.py
@@ -16,7 +16,8 @@ Implementation Notes
 
 **Hardware:**
 
-.. todo:: Add links to any specific hardware product page(s), or category page(s). Use unordered list & hyperlink rST
+.. todo:: Add links to any specific hardware product page(s), or category page(s).
+Use unordered list & hyperlink rST
    inline format: "* `Link Text <url>`_"
 
 **Software and Dependencies:**
@@ -24,28 +25,32 @@ Implementation Notes
 * Adafruit CircuitPython firmware for the supported boards:
   https://github.com/adafruit/circuitpython/releases
 
-.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies based on the library's use of either.
+.. todo:: Uncomment or remove the Bus Device and/or the Register library
+dependencies based on the library's use of either.
 
 # * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
 # * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
 # imports
-import adafruit_requests as requests
 import json
+import adafruit_requests as requests
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/2231puppy/Adafruit_CircuitPython_DiscordBot.git"
 
 
 class DiscordBot:
+    """DiscordBot Class"""
+
     def __init__(self, key, webhook):
         self.key = key
         self.r = None
-        self.jsonified = None
+        self.jsonified_r = None
         self.webhook = webhook
 
     def get_msg(self, channel, msg):
+        """Gets a message from the channel. Use 0 for latest."""
         self.r = requests.get(
             "https://discord.com/api/v8/channels/" + channel + "/messages",
             headers={"Authorization": "Bot " + self.key},
@@ -54,4 +59,5 @@ class DiscordBot:
         return self.jsonified_r[msg]["content"]
 
     def send_msg(self, msg):
+        """Sends a webhook message."""
         requests.post(self.webhook, {"content": msg})
